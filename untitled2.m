189d82c25990b5db722c46eebfd334916bc06ab3
codingPotato21 <adnan.youssef@ymail.com>
2023-05-31 16:57:50 +0400

    Initial commit
    - Here we go again :smiley:

---
+++ untitled2.m
@@ -0,0 +1,83 @@
+%% Initial setup
+
+close all
+clear all
+clc
+
+load('EEG.mat');
+
+%% Get the signals
+
+% Get the left and right EEG signals from file
+time = b1(:,1);
+left_eeg = b1(:,2);
+right_eeg = b1(:,3);
+
+%% Find the FFT
+
+% Find the shifted FFT
+n = length(left_eeg);
+signal_fft = fftshift(abs(fft(left_eeg, n)));
+frequencies = (0:n-1);
+
+% Find the center peak of the FFT
+[peaks, positions] = findpeaks(signal_fft, 'SortStr', 'descend');
+max_positions = positions(1:2);
+center = (max_positions(2) + max_positions(1)) / 2;
+
+% Find the peaks where the noise happen
+peakThresh = peaks(1) * 0.25;
+[peaks, positions] = findpeaks(signal_fft, 'MinPeakHeight', peakThresh);
+first_peak = min(positions);
+last_peak = max(positions);
+
+figure
+plot(frequencies, signal_fft)
+hold('on')
+plot([first_peak last_peak max_positions(1) max_positions(2)], [0 0 0 0], 'rv', 'MarkerFaceColor', 'b')
+hold('off')
+title("Signal Peaks")
+
+%% Center the FFT around 0
+
+% Center the shifted FFT at 0 on the x-axis
+range = (length(signal_fft) - center) * 2;
+shifted_frequencies = range*linspace(-1, 1, n);
+
+figure
+plot(shifted_frequencies, signal_fft);
+title("Shifted FFT")
+
+%% Filter the noise out using bandpass filter
+
+% Find sampling freq
+fs = n / max(time);
+
+% Filter parameters
+shift_region = fs * 0.075;
+fstop1 = (first_peak / 100) - (shift_region * 2);
+fstop2 = (first_peak / 100) + (shift_region * 2);
+fpass1 = fstop1 + (shift_region);
+fpass2 = fstop2 - (shift_region);
+
+% Find filter order
+Rp = 1; % passband ripple
+Rs = 40; % stopband attenuation
+
+Wp = [fpass1, fpass2] / (fs/2); % Normalized passband freq
+Ws = [fstop1, fstop2] / (fs/2); % Normalized stopband freq
+
+[order, Wn] = buttord(Wp, Ws, Rp, Rs);
+
+% Design the filter
+[b, a] = butter(order, Wn, 'stop');
+
+% Filter the original signal
+filtered_signal = filter(b, a, left_eeg);
+
+% Find fft of filtered signal
+filtered_fft = fftshift(abs(fft(filtered_signal, n)));
+
+figure
+plot(shifted_frequencies, filtered_fft);
+title("Filtered FFT")
